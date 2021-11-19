3f7622fe526081b2febf2f2475655049227ce739
Timothy Mastny <tim.mastny@gmail.com>
2021-11-19 13:18:08 -0600

    save

--- sass/sass.R
+++ sass/sass.R
@@ -4,12 +4,19 @@ library(tools)
 library(sass)
 library(fs)
 
+sass_to_css <- function(sass_path) {
+  sass_path %>%
+    file_path_sans_ext() %>%
+    basename() %>%
+    paste0("static/css/", ., ".css")
+}
+
 sasses <- dir_ls("sass", glob = "*.scss")
-
-csses <- sasses %>%
-	file_path_sans_ext() %>%
-	basename() %>%
-	paste0("static/css/", ., ".css")
-
 ops <- sass_options(output_style = "compressed")
-walk2(sasses, csses, ~sass(input = sass_file(.x), output = .y, options = ops))
+
+walk(sasses, 
+  ~sass(
+    input = sass_file(.), 
+    output = sass_to_css(.), 
+    options = ops, cache = FALSE
+))
